b0b84ddab882c68867dda1d0f3c1c196070a0227
Donnie Wang <donniewang@gmail.com>
2015-02-22 16:26:42 -0800

    My answer

--- chessboard.js
+++ chessboard.js
@@ -17,36 +17,23 @@
 // size = 8 and change the program so that it works for any size, outputting 
 // a grid of the given width and height.
 
-// Outputs 8x8 grid, but not easily scalable for any size
-var a = " # # # #"
-var b = "# # # # "
-for (i=0; i<4; i++) {
-  console.log(a)
-  console.log(b)
-}
-
-
-// Answer that works and my current understanding of it
-
-// Create a string named board
-var board = ""
-
-// loop through 8 times returning ??? (not sure how this translates to rows)
-for (var b=0; b<8; b++) {
-  // loop through 8 times returning either a " " or "#" (I get this part)
-  for (var a=0; a<8; a++) {
-  	// if ??? is divisible by 2 (but a+b confuses me)
-    if ((a+b) % 2 == 0) {
-      // then add " "
-      board += " ";
-      // else add "#"
+// declare a new variable line
+var line = ""
+// loop through 8 times creating a new row each time
+for (var x = 0; x < 8; x++) {
+  // for each row, loop through 8 times creating a new column each time
+  for (var y = 0; y < 8; y++) {
+  	// declare a variable total that adds the row and column #s together
+    var total = x + y
+    // if total is divisible by 2
+    if (total % 2 == 0) {
+      // then return a space
+      line += " "
     } else {
-      board += "#";
+      // otherwise return a hashtag
+      line += "#"
     }
   }
-  // start a new line
-  board += "\n"
-};
-
-// print out board
-console.log(board)
+  line += "\n"
+}
+console.log(line)
